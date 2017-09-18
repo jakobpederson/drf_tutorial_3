e6d7bac055a8c87891ff4b87e7f32a354c172931
jakobpederson <jakob.pederson@imtapps.com>
2017-09-18 15:49:42 -0500

    Adding optional format suffixes to our URLs

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,4 +1,7 @@
 from django.conf.urls import url
+
+from rest_framework.urlpatterns import format_suffix_patterns
+
 from snippets import views
 
 
@@ -6,3 +9,4 @@ urlpatterns = [
     url(r'^snippets/$', views.snippet_list),
     url(r'^snippets/(?P<pk>[0-9]+)/$', views.snippet_detail),
 ]
+urlpatters = format_suffix_patterns(urlpatterns)
--- snippets/views.py
+++ snippets/views.py
@@ -7,7 +7,7 @@ from snippets.serializers import SnippetSerializer
 
 
 @api_view(['GET', 'POST'])
-def snippet_list(request):
+def snippet_list(request, format=None):
     if request.method == 'GET':
         snippets = Snippet.objects.all()
         serializer = SnippetSerializer(snippets, many=True)
@@ -46,7 +46,7 @@ MAPPING = {
 
 
 @api_view(['GET', 'PUT', 'DELETE'])
-def snippet_detail(request, pk):
+def snippet_detail(request, pk, format=None):
     try:
         snippet = Snippet.objects.get(pk=pk)
     except Snippet.DoesNotExist:
